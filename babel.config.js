4c440b5e40846bd22a57069e8a09858ff1c4f02b
hanab11 <sxakln2020@163.com>
2023-09-13 23:58:31 +0800

    element-ui，配置按需引入

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+
+    // 以下是element-ui的按需引入配置
+    ['@babel/preset-env', { 'modules': false }]
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        'libraryName': 'element-ui',
+        'styleLibraryName': 'theme-chalk'
+      }
+    ]
   ]
 }
