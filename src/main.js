93db010bd92c2e53b6123af2364fe1181e1ddf26
hanab11 <sxakln2020@163.com>
2023-09-19 03:46:19 +0800

    引入el的表格、表格列、分页

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import router from './router'
 import App from './App.vue'
 
 // 按需引入
-import { Button, DatePicker, Row } from 'element-ui';
+import { Button, DatePicker, Row, Table, TableColumn, Pagination } from 'element-ui';
 
 // ElementUI，按需引入需要自己注册全局组件('组件名', 组件)
 Vue.component(Button.name, Button)
 Vue.component(DatePicker.name, DatePicker)
 Vue.component(Row.name, Row)
+Vue.component(Table.name, Table)
+Vue.component(TableColumn.name, TableColumn)
+Vue.component(Pagination.name, Pagination)
 
 
 Vue.config.productionTip = false
